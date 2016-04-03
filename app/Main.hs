b4aea5e51edcb998d99fd8624bcfbc5b1130e99b
Michael Smith <michael@spinda.net>
2016-04-03 12:47:26 -0700

    Complete migration to GPLv3

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,20 @@
+-- Copyright (c) 2016 Michael Smith.
+
+-- This file is part of Glyph CLI.
+
+-- Glyph CLI is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- Glyph CLI is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with Glyph CLI. If not, see <http://www.gnu.org/licenses/>.
+
 main :: IO ()
 main = putStrLn "Not yet implemented."
 
